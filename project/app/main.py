20820d7b883a9b55b1089d5e763dec2f9e4f2f0f
Nick Vandewiele <nickvandewiele@gmail.com>
2020-06-06 15:11:38 -0400

    refactor schema generation, db initialization

--- project/app/main.py
+++ project/app/main.py
@@ -1,25 +1,29 @@
-# project/app/main.py
-import os
+import logging
 
 from fastapi import FastAPI, Depends
-from tortoise.contrib.fastapi import register_tortoise
 
 from app.api import ping
+from app.db import init_db
+
+log = logging.getLogger(__name__)
+
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=True,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
 
     return application
 
 
 app = create_application()
+
+
+@app.on_event("startup")
+async def startup_event():
+    log.info("Starting up...")
+    init_db(app)
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
