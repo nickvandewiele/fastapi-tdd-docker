c5f84e01d58ab4c704d618374e45e6c85d4784b1
Nick Vandewiele <nickvandewiele@gmail.com>
2020-06-06 12:37:42 -0400

    setup docker, docker-compose, postgres, tortoise-orm

--- project/app/main.py
+++ project/app/main.py
@@ -1,11 +1,21 @@
 # project/app/main.py
-
+import os
 
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import get_settings, Settings
 
 app = FastAPI()
+
+
+register_tortoise(
+    app,
+    db_url=os.environ.get("DATABASE_URL"),
+    modules={"models": ["app.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
---
+++ project/app/models/tortoise.py
@@ -0,0 +1,13 @@
+# project/app/models/tortoise.py
+
+
+from tortoise import fields, models
+
+
+class TextSummary(models.Model):
+    url = fields.TextField()
+    summary = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
