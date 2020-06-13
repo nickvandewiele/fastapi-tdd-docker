fa2e44091c000292707496c0b0b2edab83e49f80
Nick Vandewiele <nickvandewiele@gmail.com>
2020-06-13 09:08:53 -0400

    remove asyncio sleep call

--- project/app/summarizer.py
+++ project/app/summarizer.py
@@ -1,5 +1,3 @@
-import asyncio
-
 import nltk
 from newspaper import Article
 
@@ -19,7 +17,5 @@ async def generate_summary(summary_id: int, url: str) -> str:
         article.nlp()
 
     summary = article.summary
-
-    await asyncio.sleep(10)
 
     await TextSummary.filter(id=summary_id).update(summary=summary)
